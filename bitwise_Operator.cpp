08e01bd75add7d50ad158bb8170a23ec9836f403
tarunagarwal99 <tarunprahladka965@gmail.com>
2021-07-28 21:28:30 +0530

    .

---
+++ bitwise_Operator.cpp
@@ -0,0 +1,13 @@
+//bitwise and ,or,xor
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a=3,b=6;
+    cout<<(a&b)<<endl;
+    cout<<(a|b)<<endl;
+    cout<<(a^b)<<endl;
+    cout<<(a<<1)<<endl;
+    cout<<(b>>2)<<endl;
+    return 0;
+}
