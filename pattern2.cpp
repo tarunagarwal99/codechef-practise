2eb05ced0d8b9121a95c30481d0220eeccdf4870
tarunagarwal99 <tarunprahladka965@gmail.com>
2021-08-14 21:53:36 +0530

    .

--- pattern2.cpp
+++ pattern2.cpp
@@ -1,23 +1,17 @@
-// 1
-// 0 1
-// 1 0 1
-// 0 1 0 1
-// 1 0 1 0 1
-
 #include<iostream>
 using namespace std;
 
 int main() {
     int n;
     cin>>n;
-    for (int i = 0; i < n; i++)
+    
+
+    for (int  i =n-1; i>=0; i--)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j =0; j <=i; j++)
         {
-            
-            
+           cout<<"*";
         }
-       
         cout<<endl;
         
     }
